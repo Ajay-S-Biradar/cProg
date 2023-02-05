b269eedcdabb0c8f2bf46d5e1604d6c907f0748b
Ajay-S-Biradar <sbmunnu@gmail.com>
2023-02-05 08:26:31 +0530

    Binay Tree

---
+++ binaryT.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+struct node* newNode(int data)
+{
+	// Allocate memory for new node
+	struct node* node
+		= (struct node*)malloc(sizeof(struct node));
+
+	// Assign data to this node
+	node->data = data;
+
+	// Initialize left and
+	// right children as NULL
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
+
+int main()
+{
+	/*create root*/
+	struct node* root = newNode(1);
+	newNode(2);
+	root->right = newNode(3);
+
+
+	root->left->left = newNode(4);
+
+	getchar();
+	return 0;
+}
+
